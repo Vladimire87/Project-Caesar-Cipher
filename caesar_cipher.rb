d1335c5eea0f437837563cc2484ae4b1ee7a7b95
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-03-12 01:48:46 +0400

    style rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,20 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, key)
-  caesar_cipher = ""
+  caesar_cipher = ''
   string.each_char do |l|
-    if l >= "a" && l <= "z"
-      cipher = ((l.ord - "a".ord + key) % 26 + "a".ord).chr
-    elsif l >= "A" && l <= "Z"
-      cipher = ((l.ord - "A".ord + key) % 26 + "A".ord).chr
-    else
-      cipher = l
-    end
+    cipher = if l >= 'a' && l <= 'z'
+               ((l.ord - 'a'.ord + key) % 26 + 'a'.ord).chr
+             elsif l >= 'A' && l <= 'Z'
+               ((l.ord - 'A'.ord + key) % 26 + 'A'.ord).chr
+             else
+               l
+             end
     caesar_cipher << cipher
   end
   caesar_cipher
 end
+
+puts caesar_cipher(
+  "Let's take it one step further\nand print out just the favorite ice cream flavors.\nOnce again, we'll need to iterate down into that level of the hash,\nthen we can access the favorite ice cream array and print out the flavors:", 2
+)
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,39 +1,41 @@
-require_relative '../caesar_cipher.rb'
+# frozen_string_literal: true
 
-describe "#caesar_cipher" do
-  it "shifts lowercase letters by the key value" do
-    expect(caesar_cipher("hello", 3)).to eq("khoor")
+require_relative '../caesar_cipher'
+
+describe '#caesar_cipher' do
+  it 'shifts lowercase letters by the key value' do
+    expect(caesar_cipher('hello', 3)).to eq('khoor')
   end
-  
-  it "shifts uppercase letters by the key value" do
-    expect(caesar_cipher("HELLO", 3)).to eq("KHOOR")
+
+  it 'shifts uppercase letters by the key value' do
+    expect(caesar_cipher('HELLO', 3)).to eq('KHOOR')
   end
-  
-  it "wraps around the alphabet for lowercase letters" do
-    expect(caesar_cipher("xyz", 3)).to eq("abc")
+
+  it 'wraps around the alphabet for lowercase letters' do
+    expect(caesar_cipher('xyz', 3)).to eq('abc')
   end
-  
-  it "wraps around the alphabet for uppercase letters" do
-    expect(caesar_cipher("XYZ", 3)).to eq("ABC")
+
+  it 'wraps around the alphabet for uppercase letters' do
+    expect(caesar_cipher('XYZ', 3)).to eq('ABC')
   end
-  
-  it "leaves non-alphabetic characters unchanged" do
-    expect(caesar_cipher("hello!", 3)).to eq("khoor!")
+
+  it 'leaves non-alphabetic characters unchanged' do
+    expect(caesar_cipher('hello!', 3)).to eq('khoor!')
   end
-  
-  it "handles negative key values" do
-    expect(caesar_cipher("khoor", -3)).to eq("hello")
+
+  it 'handles negative key values' do
+    expect(caesar_cipher('khoor', -3)).to eq('hello')
   end
-  
-  it "handles key values greater than 26" do
-    expect(caesar_cipher("hello", 30)).to eq("lipps")
+
+  it 'handles key values greater than 26' do
+    expect(caesar_cipher('hello', 30)).to eq('lipps')
   end
-  
-  it "handles empty strings" do
-    expect(caesar_cipher("", 5)).to eq("")
+
+  it 'handles empty strings' do
+    expect(caesar_cipher('', 5)).to eq('')
   end
-  
-  it "handles keys of 0" do
-    expect(caesar_cipher("hello", 0)).to eq("hello")
+
+  it 'handles keys of 0' do
+    expect(caesar_cipher('hello', 0)).to eq('hello')
   end
 end
